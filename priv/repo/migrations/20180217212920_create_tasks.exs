c315c26d5de203b2ee786347de47a1867e881553
Hulnis <eric.parker13@gmail.com>
2018-02-18 14:03:11 -0500

    actually add feed

--- priv/repo/migrations/20180217212920_create_tasks.exs
+++ priv/repo/migrations/20180217212920_create_tasks.exs
@@ -12,6 +12,7 @@ defmodule Tracker.Repo.Migrations.CreateTasks do
       timestamps()
     end
 
-    create index(:tasks, [:assigned_user])
+    create index(:tasks, [:assigned_user_id])
+    create index(:tasks, [:user_id])
   end
 end
