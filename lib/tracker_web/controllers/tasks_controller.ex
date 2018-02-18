46fd5991c6a71274e3536b4039c7544e7db9dcf2
Hulnis <eric.parker13@gmail.com>
2018-02-18 15:19:44 -0500

    actually add feed

--- lib/tracker_web/controllers/tasks_controller.ex
+++ lib/tracker_web/controllers/tasks_controller.ex
@@ -16,6 +16,7 @@ defmodule TrackerWeb.TasksController do
 
   def create(conn, %{"tasks" => tasks_params}) do
     IO.inspect(tasks_params)
+    tasks_params = Map.put(tasks_params, "assigned_user_id", 1)
     # tasks_params = Map.put(tasks_params, "user_id", conn.assigns[:current_user].id)
     # IO.inspect(tasks_params)
     case Social.create_tasks(tasks_params) do
