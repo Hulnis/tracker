10520cb7f9d7e7e273dd1894a0a4c74cce8d2796
Hulnis <eric.parker13@gmail.com>
2018-02-20 16:36:05 -0500

    improve from and create

--- lib/tracker_web/controllers/tasks_controller.ex
+++ lib/tracker_web/controllers/tasks_controller.ex
@@ -56,7 +56,8 @@ defmodule TrackerWeb.TasksController do
     IO.inspect(tasks.user.id)
     IO.inspect(conn.assigns[:current_user].id)
     tasks_params = if (conn.assigns[:current_user].id == tasks.user.id && name != "") do
-       Map.put(tasks_params, "assigned_user_id", Accounts.get_user_by_name(name).id)
+      IO.puts("in the if")
+      Map.put(tasks_params, "assigned_user_id", Accounts.get_user_by_name(name).id)
     else
       tasks_params
     end
