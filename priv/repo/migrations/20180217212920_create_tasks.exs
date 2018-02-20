8820c50eb0c25ecd58a415fc95cdd4884a9a6112
Hulnis <eric.parker13@gmail.com>
2018-02-20 16:27:13 -0500

    improve from and create

--- priv/repo/migrations/20180217212920_create_tasks.exs
+++ priv/repo/migrations/20180217212920_create_tasks.exs
@@ -7,7 +7,7 @@ defmodule Tracker.Repo.Migrations.CreateTasks do
       add :title, :string, null: false
       add :time_spent, :integer, default: 0
       add :user_id, references(:users, on_delete: :delete_all), null: false
-      add :assigned_user_id, references(:users, on_delete: :delete_all), default: nil
+      add :assigned_user_id, references(:users), default: nil
       add :is_complete, :boolean, default: false
 
       timestamps()
