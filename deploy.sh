d419152a26c70241d13523f6b5a5f01b9a985149
Hulnis <eric.parker13@gmail.com>
2018-02-20 19:06:22 -0500

    improve from and create

--- deploy.sh
+++ deploy.sh
@@ -21,24 +21,24 @@ mix deps.get
 (cd assets && npm install)
 (cd assets && ./node_modules/brunch/bin/brunch b -p)
 # mix ecto.create
-# mix ecto.migrate
+mix ecto.migrate
 mix phx.digest
-mix release --env=prod
+# mix release --env=prod
 
-# PORT=5102 MIX_ENV=prod elixir --detached -S mix phx.server
+PORT=5102 MIX_ENV=prod elixir --detached -S mix phx.server
 
-mkdir -p ~/www
-mkdir -p ~/old
-
-NOW=`date +%s`
-if [ -d ~/www/tracker ]; then
-	echo mv ~/www/tracker ~/old/$NOW
-	mv ~/www/tracker ~/old/$NOW
-fi
-
-mkdir -p ~/www/tracker
-REL_TAR=~/src/tracker/_build/prod/rel/tracker/releases/0.0.1/tracker.tar.gz
-(cd ~/www/tracker && tar xzvf $REL_TAR)
+# mkdir -p ~/www
+# mkdir -p ~/old
+#
+# NOW=`date +%s`
+# if [ -d ~/www/tracker ]; then
+# 	echo mv ~/www/tracker ~/old/$NOW
+# 	mv ~/www/tracker ~/old/$NOW
+# fi
+#
+# mkdir -p ~/www/tracker
+# REL_TAR=~/src/tracker/_build/prod/rel/tracker/releases/0.0.1/tracker.tar.gz
+# (cd ~/www/tracker && tar xzvf $REL_TAR)
 
 crontab - <<CRONTAB
 @reboot bash /home/tracker/src/tracker/start.sh
