ed9059e2a9ed9372b59dc0b3633ca122dfc3ab56
Hulnis <eric.parker13@gmail.com>
2018-02-25 14:22:14 -0500

    fixing errors 1

--- priv/repo/migrations/20180217211333_create_users.exs
+++ priv/repo/migrations/20180217211333_create_users.exs
@@ -5,6 +5,7 @@ defmodule Tracker.Repo.Migrations.CreateUsers do
     create table(:users) do
       add :email, :string, null: false
       add :name, :string, null: false
+      add :managed_by, references(:users, on_delete: :delete_all), default: nil
 
       timestamps()
     end
