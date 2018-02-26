06181b0fe2075cf99173d112a6d4723b46bfd7ee
Hulnis <eric.parker13@gmail.com>
2018-02-26 18:18:29 -0500

    updating manage

--- lib/tracker/social/tasks.ex
+++ lib/tracker/social/tasks.ex
@@ -13,7 +13,7 @@ defmodule Tracker.Social.Tasks do
     belongs_to :assigned_user, Tracker.Accounts.User
     belongs_to :user, Tracker.Accounts.User
 
-    has_many :timeblocks, TimeBlock, foreign_key: :task_id
+    has_many :timeblocks, TimeBlock
 
     timestamps()
   end
