759b2fe8ae2b777d813a2beae9f7b51bacfcc84c
Hulnis <eric.parker13@gmail.com>
2018-02-26 18:08:12 -0500

    updating manage

---
+++ priv/repo/migrations/20180226212920_create_tasks.exs
@@ -0,0 +1,21 @@
+defmodule Tracker.Repo.Migrations.CreateTasks do
+  use Ecto.Migration
+
+  def change do
+    create table(:tasks) do
+      add :body, :text, null: false
+      add :title, :string, null: false
+      add :time_spent, :integer, default: 0
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+      add :assigned_user_id, references(:users), default: nil
+      add :is_complete, :boolean, default: false
+      add :time_blocks, references(:timeblocks, on_delete: :delete_all)
+
+      timestamps()
+    end
+
+    create index(:tasks, [:assigned_user_id])
+    create index(:tasks, [:user_id])
+    create index(:tasks, [:timeblocks])
+  end
+end
