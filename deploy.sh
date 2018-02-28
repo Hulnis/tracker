246eff1e49e481bdf9ce681ee57c5067e1a85ed3
Hulnis <eric.parker13@gmail.com>
2018-02-27 20:49:34 -0500

    new time block

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,20 @@
 
 export PORT=5103
 export MIX_ENV=prod
-# export GIT_PATH=/home/tracker2/src/tracker
-#
-# PWD=`pwd`
-# if [ $PWD != $GIT_PATH ]; then
-# 	echo "Error: Must check out git repo to $GIT_PATH"
-# 	echo "  Current directory is $PWD"
-# 	exit 1
-# fi
-#
-# if [ $USER != "tracker" ]; then
-# 	echo "Error: must run as user 'tracker'"
-# 	echo "  Current user is $USER"
-# 	exit 2
-# fi
+export GIT_PATH=/home/tracker2/src/tracker
+
+PWD=`pwd`
+if [ $PWD != $GIT_PATH ]; then
+	echo "Error: Must check out git repo to $GIT_PATH"
+	echo "  Current directory is $PWD"
+	exit 1
+fi
+
+if [ $USER != "tracker" ]; then
+	echo "Error: must run as user 'tracker'"
+	echo "  Current user is $USER"
+	exit 2
+fi
 
 mix deps.get
 (cd assets && npm install)
@@ -23,25 +23,25 @@ mix deps.get
 # mix ecto.create
 mix ecto.migrate
 mix phx.digest
-# mix release --env=prod
+mix release --env=prod
 
-PORT=5103 MIX_ENV=prod elixir --detached -S mix phx.server
+# PORT=5103 MIX_ENV=prod elixir --detached -S mix phx.server
 
-# mkdir -p ~/www
-# mkdir -p ~/old
-#
-# NOW=`date +%s`
-# if [ -d ~/www/tracker ]; then
-# 	echo mv ~/www/tracker ~/old/$NOW
-# 	mv ~/www/tracker ~/old/$NOW
-# fi
-#
-# mkdir -p ~/www/tracker
-# REL_TAR=~/src/tracker/_build/prod/rel/tracker/releases/0.0.1/tracker.tar.gz
-# # (cd ~/www/tracker && tar xzvf $REL_TAR)
-#
-# crontab - <<CRONTAB
-# @reboot bash /home/tracker/src/tracker/start.sh
-# CRONTAB
+mkdir -p ~/www
+mkdir -p ~/old
 
-#. start.sh
+NOW=`date +%s`
+if [ -d ~/www/tracker ]; then
+	echo mv ~/www/tracker ~/old/$NOW
+	mv ~/www/tracker ~/old/$NOW
+fi
+
+mkdir -p ~/www/tracker
+REL_TAR=~/src/tracker/_build/prod/rel/tracker/releases/0.0.1/tracker.tar.gz
+(cd ~/www/tracker && tar xzvf $REL_TAR)
+
+crontab - <<CRONTAB
+@reboot bash /home/tracker/src/tracker/start.sh
+CRONTAB
+
+. start.sh
