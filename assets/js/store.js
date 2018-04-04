1f05ed7a8bf15765664c32a7b02b121d2b51ae21
Hulnis <eric.parker13@gmail.com>
2018-04-04 17:46:20 -0400

    task update

--- assets/js/store.js
+++ assets/js/store.js
@@ -54,7 +54,7 @@ function edit_task_form(state = empty_task_update_form, action) {
       if(state[action.data.id]) {
         return Object.assign({}, state[action.data.id], action.data.update)
       } else {
-        Object.assign({}, state, {action.data.id: {}})
+        state = Object.assign({}, state, {action.data.id: {}})
         return Object.assign({}, state[action.data.id], action.data.update)
       }
     case 'SET_TOKEN':
