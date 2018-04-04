83faca7abf763fb1cffd1d2629557bc7b622c17a
Hulnis <eric.parker13@gmail.com>
2018-04-03 22:17:50 -0400

    don't copy paste kids

--- lib/tracker_web/controllers/page_controller.ex
+++ lib/tracker_web/controllers/page_controller.ex
@@ -4,10 +4,4 @@ defmodule TrackerWeb.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
-  # 
-  # def feed(conn, _params) do
-  #   tasks = Tracker.Social.list_tasks()
-  #   changeset = Tracker.Social.change_tasks(%Tracker.Social.Tasks{})
-  #   render conn, "feed.html", tasks: tasks, changeset: changeset
-  # end
 end
