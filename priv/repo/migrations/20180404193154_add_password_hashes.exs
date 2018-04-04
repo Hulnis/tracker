c5efe63ff936b704c79b51ec94fd63e814230862
Hulnis <eric.parker13@gmail.com>
2018-04-04 15:37:12 -0400

    migrate update

--- priv/repo/migrations/20180404193154_add_password_hashes.exs
+++ priv/repo/migrations/20180404193154_add_password_hashes.exs
@@ -2,6 +2,8 @@ defmodule Tracker.Repo.Migrations.AddPasswordHashes do
   use Ecto.Migration
 
   def change do
-
-  end
+      alter table("users") do
+        add :password_hash, :string
+      end
+    end
 end
