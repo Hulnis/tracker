f1dcdf546b858588451d3a1c61b6f17c50216cf6
Hulnis <eric.parker13@gmail.com>
2018-04-04 18:09:37 -0400

    task update

--- assets/js/store.js
+++ assets/js/store.js
@@ -54,12 +54,12 @@ function edit_task_form(state = empty_task_update_form, action) {
       return Object.assign({}, state[action.data.id], action.data.update)
     case "TASKS_LIST":
       action.tasks.forEach(function(task) {
-        Object.assign({}, state, {
+        state = Object.assign({}, state, {
           time_spent: task.time_spent,
           assigned_user: task.assigned_user ? task.assigned_user.name : ""
         }
       })
-      console.log("state1")
+      console.log("state tasks list", state)
       return state
     case "ADD_TASK":
       state[task.id] = {
