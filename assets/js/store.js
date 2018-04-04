f87b36bc339f9894c2ab552352906b440290d0e6
Hulnis <eric.parker13@gmail.com>
2018-04-03 21:47:02 -0400

    don't copy paste kids

--- assets/js/store.js
+++ assets/js/store.js
@@ -82,7 +82,7 @@ function root_reducer(state0, action) {
   console.log("reducer", action)
   // {posts, users, form} is ES6 shorthand for
   // {posts: posts, users: users, form: form}
-  let reducer = combineReducers({TASKS, users, form, token, login})
+  let reducer = combineReducers({tasks, users, form, token, login})
   let state1 = reducer(state0, action)
   console.log("state1", state1)
   return deepFreeze(state1)
