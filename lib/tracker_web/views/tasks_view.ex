ad19221929df7582975565dc43127089ebeab790
Hulnis <eric.parker13@gmail.com>
2018-04-03 22:25:30 -0400

    don't copy paste kids

--- lib/tracker_web/views/tasks_view.ex
+++ lib/tracker_web/views/tasks_view.ex
@@ -1,3 +1,25 @@
 defmodule TrackerWeb.TasksView do
   use TrackerWeb, :view
+  alias TrackerWeb.TasksView
+  alias TrackerWeb.UserView
+
+  def render("index.json", %{tasks: tasks}) do
+    %{data: render_many(tasks, TasksView, "tasks.json")}
+  end
+
+  def render("show.json", %{tasks: tasks}) do
+    %{data: render_one(tasks, TasksView, "tasks.json")}
+  end
+
+  def render("tasks.json", %{tasks: tasks}) do
+    %{
+      id: tasks.id,
+      body: tasks.body,
+      title: tasks.body,
+      is_complete: tasks.is_complete,
+      time_spent: tasks.time_spent,
+      assigned_user: tasks.assigned_user,
+      user: render_one(tasks.user, UserView, "user.json")
+    }
+  end
 end
