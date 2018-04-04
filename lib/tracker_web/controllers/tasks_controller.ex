a914dd4b14e8f4c4c18b9d9100844a35a9c503d0
Hulnis <eric.parker13@gmail.com>
2018-04-04 19:16:54 -0400

    task update

--- lib/tracker_web/controllers/tasks_controller.ex
+++ lib/tracker_web/controllers/tasks_controller.ex
@@ -69,7 +69,7 @@ defmodule TrackerWeb.TasksController do
     if rem(time_spent, 15) == 0 and tasks.assigned_user != nil and user_id == tasks.assigned_user.id do
       IO.puts("yes time")
       tasks_params = Map.put(tasks_params, "time_spent", time_spent)
-      with {:ok, %Tasks{} = tasks} <- Social.update_tasks(tasks_params) do
+      with {:ok, %Tasks{} = tasks} <- Social.update_tasks(tasks, asks_params) do
           render(conn, "show.json", tasks: tasks)
       end
     else
