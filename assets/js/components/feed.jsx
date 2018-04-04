a951867ba4134dc95a583172c5b28ea8887e857b
Hulnis <eric.parker13@gmail.com>
2018-04-04 16:46:55 -0400

    task creation

--- assets/js/components/feed.jsx
+++ assets/js/components/feed.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Post from './post'
 
 export default function Feed(params) {
+  console.log("feed params", params)
   let tasks = _.map(params.tasks, (pp) => <Task key={pp.id} post={pp} />)
   return (
     <div>
