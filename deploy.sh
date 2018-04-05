638c66838ed88fc651e5985b8b04e0e5beeca971
Hulnis <eric.parker13@gmail.com>
2018-04-04 20:21:48 -0400

    task update

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,6 @@ export PORT=5102
 export MIX_ENV=prod
 export GIT_PATH=/home/tracker/src/tracker
 
-PWD=`pwd`
-if [ $PWD != $GIT_PATH ]; then
-	echo "Error: Must check out git repo to $GIT_PATH"
-	echo "  Current directory is $PWD"
-	exit 1
-fi
-
-if [ $USER != "tracker" ]; then
-	echo "Error: must run as user 'tracker'"
-	echo "  Current user is $USER"
-	exit 2
-fi
 
 mix deps.get
 (cd assets && npm install)
